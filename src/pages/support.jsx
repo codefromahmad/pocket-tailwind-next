a5d8a16eeae0dd92b4717d0dd999fda322060d2e
Arsalan Ahmad <you@example.com>
2022-10-29 04:16:45 +0500

    Searching functionality part 1

--- src/pages/support.jsx
+++ src/pages/support.jsx
@@ -1,8 +1,9 @@
 import { Footer } from '@/components/Footer'
 import { Header } from '@/components/Header'
 import Link from 'next/link'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { BiSearch } from 'react-icons/bi'
+import { GrFormClose } from 'react-icons/gr'
 
 const Support = () => {
   const supportLinks = [
@@ -198,41 +199,87 @@ const Support = () => {
   ]
 
   const [search, setSearch] = useState('')
-  const [result, setResult] = useState()
+  const [result, setResult] = useState('')
 
-  const handleChange = (e) => {
-    setSearch(e.target.value)
+  const getData = (search) => {
     const data = supportLinks.map((item) => ({
       links: item.links.filter((child) =>
         child.title.toLowerCase().includes(search.toLowerCase())
       ),
     }))
     setResult(data)
-    console.log(result)
+  }
+
+  const handleChange = (e) => {
+    setSearch(e.target.value)
+  }
+
+  useEffect(() => {
+    console.log('Inside useEffect printing result : ' + search.toLowerCase())
+    console.log('====================================')
+    if (result) {
+      result.map((item) => item.links.map((i) => console.log(i.title)))
+    }
+    console.log('====================================')
+  }, [result])
+
+  useEffect(() => {
+    getData(search)
+  }, [search])
+
+  const handleClose = () => {
+    setResult('')
+    setSearch('')
   }
 
   return (
     <div>
       <Header />
-      <div className="w-full">
+      <div className="w-full" onClick={handleClose}>
         <div className="mx-auto max-w-screen-lg px-5">
           <div>
             <h1 className="text-4xl font-bold">Hello!</h1>
             <h1 className="text-4xl font-bold">How can we help?</h1>
-            <div className="group">
-              <div className="my-5 flex max-w-screen-sm items-center rounded-full bg-slate-200 py-1 text-xl duration-300 group-focus-within:bg-slate-100 group-focus-within:shadow-xl">
+            <div className="group relative">
+              <div
+                className={` ${
+                  result && search && 'bg-slate-100 shadow-xl'
+                } mt-5 flex max-w-screen-sm items-center ${
+                  search && result
+                    ? 'rounded-t-2xl border-b-2 border-black'
+                    : 'rounded-full'
+                } bg-slate-200 py-1 text-xl group-focus-within:bg-slate-50 group-focus-within:shadow-xl`}
+              >
                 <span className="pl-4">
                   <BiSearch className="text-red text-2xl text-[rgb(106,114,120)]" />
                 </span>
                 <input
                   type="text"
+                  autoComplete="off"
                   name="search"
                   onChange={handleChange}
                   value={search}
                   placeholder="Search"
                   className="w-full border-none bg-transparent text-xl focus:ring-0"
                 />
-                {/* {result && result.map((item) => console.log(item.links))} */}
+                {result && search && (
+                  <span className="mr-4 cursor-pointer rounded-full duration-300 ease-in-out hover:bg-slate-300">
+                    <GrFormClose size={24} onClick={handleClose} />
+                  </span>
+                )}
+              </div>
+              <div className="z-200 absolute max-h-[385px] w-[640px] overflow-hidden rounded-b-2xl bg-slate-50 shadow-xl">
+                {search &&
+                  result &&
+                  result.map((item) =>
+                    item.links.map((data) => (
+                      <Link href={`${data.link}`} className="">
+                        <p className="max-w-screen-sm cursor-pointer py-2 pl-4 text-lg text-gray-800 duration-200 hover:bg-slate-100 ">
+                          {data.title}
+                        </p>
+                      </Link>
+                    ))
+                  )}
               </div>
             </div>
           </div>
